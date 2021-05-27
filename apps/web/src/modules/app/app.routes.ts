a4eba6cca2346b5404c660d82f241d87d43c44cf
Samyadh <samyadh.jain@gmail.com>
2021-05-27 13:09:18 +0530

    inital commit

--- apps/web/src/modules/app/app.routes.ts
+++ apps/web/src/modules/app/app.routes.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: "search",
+        path: "me",
+        loadChildren: () => import("../portfolio/portfolio.module").then(m => m.PortfolioModule)
+    },
+    {
+        path: "vaccine",
         loadChildren: () => import("../search-slot/search-slot.module").then(m => m.SearchSlotModule)
     },
     {
         path: "**",
-        redirectTo: "search"
+        redirectTo: "me"
     }
 ];
 
