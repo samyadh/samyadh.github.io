2bb7de3a75dc9b4b517093017d55e847efed3f8c
Samyadh <samyadh.jain@gmail.com>
2021-05-27 17:02:52 +0530

    routing issue solved

--- apps/web/src/modules/app/components/app-component/app.component.ts
+++ apps/web/src/modules/app/components/app-component/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Vaccine Slot Finder';
+    title = 'Vaccine Slot Finder';
+
+    constructor(private router: Router) {
+        let path = localStorage.getItem('path');
+        if (path) {
+            localStorage.removeItem('path');
+            this.router.navigate([path]);
+        }
+    }
 }
