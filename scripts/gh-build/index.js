557728f4526102dc4f2b68eb85bc30df7e8f56b1
Samyadh <samyadh.jain@gmail.com>
2021-05-27 14:25:48 +0530

    build scripts updated

---
+++ scripts/gh-build/index.js
@@ -0,0 +1,18 @@
+const exec = require("child_process").exec;
+const fs = require('fs');
+
+exec("ng build", function(err, stdout, stderr) {
+    if (err) {
+        console.log(err);
+    }
+    fs.copyFile("scripts/gh-build/404.html","docs/404.html",(err) => {
+    if (err) {
+        console.log("Error Found:", err);
+    }
+    else {
+        console.log("\nBuild complete. \n");
+    }
+    });
+});
+
+
