df5d97e9dc840eb46734242b3321b54fdf5d8b9c
samyadh <samyadh.jain@gmail.com>
2021-06-20 04:40:57 +0530

    setup done

---
+++ apps/portfolio/src/app/app.tsx
@@ -0,0 +1,14 @@
+import styles from './app.module.scss';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
